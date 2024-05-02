31bccee8efeada285981c09e07bfdbb080f12579
Gulbeyaz <glbzbayram@gmail.com>
2024-05-03 02:46:03 +0300

    main.py for fastapi

---
+++ fastApi/main.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI, Request, File, UploadFile 
+from pydantic import BaseModel # Fonksiyonlara gelen parametreleri kontrol etmek için
+from tensorflow import keras
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from fastapi.responses import  HTMLResponse
+from PIL import Image
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    with open("C:/Users/Hp/Documents/GitHub/image_classification_cnn/fastApi/index.html", encoding="utf-8") as file:
+        content = file.read()
+    return HTMLResponse(content)
+
+class model_input(BaseModel):
+    Unnamed: int
+    Age: int
+    Gender: int
+    B0I: int
+    Chol: float
+    TG: float
+    HDL: float
+    LDL: float
+    Cr: float
+    BUN : float
+
+diabetes_model=load_model('models/model.h5')
+
+@app.post("/predict_diabetes")
+def predict(input_parameters : model_input ):
+     data = pd.DataFrame([input_parameters.model_dump()])
+     sc = StandardScaler()
+    
+     sc.fit(data)
+     data = sc.transform(data)
+     
+     prediction = diabetes_model.predict(data)
+     print(prediction)
+
+     if prediction[0][0] == 1.0:
+         return {"message": "The person is Diabetic"}
+     else:
+         return {"message": "The person is not Diabetic"}
+
+
+@app.post("/predict_image")
+async def predict_image(file: UploadFile = File(...)):
+    load_cnn_model = load_model(r'C:\Users\Hp\Documents\GitHub\image_classification_cnn\fastApi\models\model_cnn.h5')
+    with open("frame12.jpg", "wb") as f:
+        f.write(await file.read())
+        
+    test_image = Image.open("frame12.jpg")
+    test_image = test_image.resize((240, 240))
+    
+    test_image = np.array(test_image)
+    test_image = np.expand_dims(test_image, axis=0) / 255.0
+    
+    prediction = load_cnn_model.predict(test_image)
+    # predicted_class_index = np.argmax(prediction[0][0])
+
+    if prediction[0][0] <= 0.5:
+        return 'Jerry'
+    else:
+         return 'Tom'
+
+
+
